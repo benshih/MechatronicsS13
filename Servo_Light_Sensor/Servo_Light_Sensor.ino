309da2a692685566001adadd56bbca340b40d8f2
Mark Erazo <alastor5587@gmail.com>
2013-02-10 23:58:35 -0500

    Working Light Sensor

--- Servo_Light_Sensor/Servo_Light_Sensor.ino
+++ Servo_Light_Sensor/Servo_Light_Sensor.ino
@@ -6,8 +6,9 @@ Servo myservo;  // create servo object to control a servo
                 // a maximum of eight servo objects can be created 
  
 int sensorPin = A1; //selected A1 analog input to control servo using light sensor
-int sensorDelay = 2000; //time to wait before next step of code (1000) was old time
+int sensorDelay = 100; //time to wait before next step of code (1000) was old time
 int sensorValue = 0; // variable to store the value coming from the sensor
+float servoLoc = 180 ;
  
 void setup() 
 { 
@@ -20,7 +21,13 @@ void loop()
 {
   // read the value from the sensor:
   sensorValue = analogRead(sensorPin);
+  // delays by .1 seconds
   delay(sensorDelay);
-  Serial.println(sensorValue); 
+  // scales sensor readings to servo angle
+  servoLoc = sensorValue*1.8; 
+  // writes the calculated servo location to the servo
+  myservo.write(servoLoc); 
+  // prints out servo location
+  Serial.println(servoLoc);
   delay(sensorDelay);
 }
