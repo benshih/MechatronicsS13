ccf616e0bf20e682d86e47e91a41cacb62919774
Mark Erazo <alastor5587@gmail.com>
2013-02-10 22:58:28 -0500

    First Test Compile

--- Servo_Light_Sensor/Servo_Light_Sensor.ino
+++ Servo_Light_Sensor/Servo_Light_Sensor.ino
@@ -1,11 +1,13 @@
-// RC Servo only.
+// RC Servo plus light sensor control
 
 #include <Servo.h> 
  
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
  
-int pos = 0;    // variable to store the servo position 
+int sensorPin = A1; //selected A1 analog input to control servo using light sensor
+int sensorDelay = 2000; //time to wait before next step of code (1000) was old time
+int sensorValue = 0; // variable to store the value coming from the sensor
  
 void setup() 
 { 
@@ -15,10 +17,9 @@ void setup()
  
 void loop() 
 {
-  myservo.write(0); // Set to one extreme.
-  delay(1000);
-  myservo.write(180); // Set to other extreme.
-  delay(1000);
-  myservo.write(90); // Set to variable servo.
-  delay(1000);
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  delay(sensorDelay);
+  Serial.println(sensorValue) 
+  delay(sensorDelay);
 }
