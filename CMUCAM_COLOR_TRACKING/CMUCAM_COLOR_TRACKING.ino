737816f054f9b34d80653e2952e3fa08fd751915
Ram Muthiah <rammuthiah@gmail.com>
2013-02-26 00:02:27 -0500

    Trying to track brown (Ram's Skin Color)

--- CMUCAM_COLOR_TRACKING/CMUCAM_COLOR_TRACKING.ino
+++ CMUCAM_COLOR_TRACKING/CMUCAM_COLOR_TRACKING.ino
@@ -1,19 +1,21 @@
 #include <CMUcam4.h>
 #include <CMUcom4.h>
 
-#define RED_MIN 230
-#define RED_MAX 255
-#define GREEN_MIN 230
-#define GREEN_MAX 255
-#define BLUE_MIN 230
-#define BLUE_MAX 255
+#define RED_MIN 40
+#define RED_MAX 220
+#define GREEN_MIN 0
+#define GREEN_MAX 110
+#define BLUE_MIN 0
+#define BLUE_MAX 60
 #define LED_BLINK 5 // 5 Hz
 #define WAIT_TIME 5000 // 5 seconds
+#define BAUD_RATE 9600
 
 CMUcam4 cam(CMUCOM4_SERIAL3);
 
 void setup()
 {
+  Serial.begin(BAUD_RATE);
   cam.begin();
 
   // Wait for auto gain and auto white balance to run.
@@ -35,12 +37,8 @@ void loop()
 
   cam.trackColor(RED_MIN, RED_MAX, GREEN_MIN, GREEN_MAX, BLUE_MIN, BLUE_MAX);
 
-  for(;;)
-  {
-    cam.getTypeTDataPacket(&data); // Get a tracking packet.
-
-    // Process the packet data safely here.
-  }
-
-  // Do something else here.
+  cam.getTypeTDataPacket(&data); // Get a tracking packet.
+  Serial.print(data.mx);
+  Serial.print(",");
+  Serial.println(data.my);
 }
