80a72db6a21dc96e56644fa24dc15902d2054c89
Ram Muthiah <rammuthiah@gmail.com>
2013-02-26 19:44:13 -0500

    outputs mostly accurate a + bx
    whoever wants to use this add the library that I emailed out to
    everyone to the Arduino folder

--- CMUCAM_LINE_TRACKING/CMUCAM_LINE_TRACKING.ino
+++ CMUCAM_LINE_TRACKING/CMUCAM_LINE_TRACKING.ino
@@ -1,3 +1,4 @@
+#include <linreg.h>
 #include <CMUcam4.h>
 #include <CMUcom4.h>
 
@@ -10,54 +11,91 @@
 #define LED_BLINK 5 // 5 Hz
 #define WAIT_TIME 5000 // 5 seconds
 #define BAUD_RATE 9600
+#define NOISE_FILTER 2
 
 CMUcam4 cam(CMUCOM4_SERIAL3);
-
-#define X1 0
-#define Y1 0
-#define X2 X1+79
-#define Y2 119
-#define X3 X2+1
-#define Y3 Y1
-#define X4 X3+79
-#define Y4 Y2
-
-int cent_x1,cent_x2,cent_x3,cent_x4,cent_x5,cent_x6,cent_x7,cent_x8;
-int cent_y1,cent_y2,cent_y3,cent_y4,cent_y5,cent_y6,cent_y7,cent_y8;
-CMUcam4_tracking_data_t data;
+int error;
+double A,B;
 
 void setup()
 {
   Serial.begin(BAUD_RATE);
-  cam.begin();
+  error = cam.begin();
+
+  if(error < CMUCAM4_RETURN_SUCCESS)
+  {
+    Serial.print("CMUCam Fail with error ");
+    Serial.println(error);
+    while(1) {}
+  }
 
   // Wait for auto gain and auto white balance to run.
-
   cam.LEDOn(LED_BLINK);
   delay(WAIT_TIME);
 
-  // Turn auto gain and auto white balance off.
+  // Turn on Poll Mode and Line Mode
+  cam.pollMode(true);
+  cam.lineMode(true); 
 
+  // Turn on Noise Filter
+  cam.noiseFilter(NOISE_FILTER);
+
+  // Turn auto gain and auto white balance off.
   cam.autoGainControl(false);
   cam.autoWhiteBalance(false);
+  
+  // Set Tracking Parameters
+  cam.setTrackingParameters(RED_MIN, RED_MAX, GREEN_MIN, GREEN_MAX, BLUE_MIN, BLUE_MAX);
+  
+  // Indicate all initialization is finished
   cam.LEDOn(CMUCAM4_LED_ON);
 }
 
 void loop()
 {
-  print(X1,Y1,X4,Y4);
-  print(X1,Y1,X2,Y2);
-  print(X3,Y3,X4,Y4);
-  
-  Serial.println();
+  track_line();
 }
 
-void print(int x_low, int y_low, int x_high, int y_high)
+void track_line()
 {
-  cam.setTrackingWindow(x_low,y_low,x_high,y_high);
-  cam.trackColor(RED_MIN, RED_MAX, GREEN_MIN, GREEN_MAX, BLUE_MIN, BLUE_MAX);
-  cam.getTypeTDataPacket(&data);
-  Serial.print(data.mx);
-  Serial.print(",");
-  Serial.println(data.my);
+  LinearRegression lr;
+  CMUcam4_tracking_data_t packetT;
+  CMUcam4_image_data_t packetF;
+  int numPixels = 0;
+  
+  cam.trackColor();
+  cam.getTypeTDataPacket(&packetT);
+  cam.getTypeFDataPacket(&packetF);
+  
+  if(packetT.pixels)
+  {
+    for(int y = 0; y < CMUCAM4_BINARY_V_RES; y++)
+    {
+      for(int x = 0; x < CMUCAM4_BINARY_H_RES; x++)
+      {
+        if(cam.getPixel(&packetF, y, x))
+        {
+          lr.addXY(x, y);
+          numPixels++;
+        }
+      }
+    }
+    
+    Serial.print("Number of pixels tracked is ");
+    Serial.println(numPixels);
+    
+    if(lr.haveData())
+    {   
+      A = lr.getA();
+      B = lr.getB();
+
+      Serial.print(A);
+      Serial.print(" + ");
+      Serial.print(B);
+      Serial.println("x");
+    }
+    
+    Serial.println();
+  }
 }
+
